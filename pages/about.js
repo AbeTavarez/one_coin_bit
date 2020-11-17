cff3b10ecbce1354e2af238f552238e3cd85f7be
Abraham E. Tavarez <abetavarez@icloud.com>
2020-11-17 00:28:57 -0500

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Navbar from '../components/Navbar';
+const About = () => (
+  <div>
+    <Navbar />
+    <h1>About | OneCoinBit</h1>
+    <p>Application to monitor One Bit of a BitCoin.</p>
+  </div>
+);
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Navbar from '../components/Navbar';
+
+const Index = () => (
+  <div>
+    <Navbar />
+    <h1>OneCoinBit | HomePage</h1>
+  </div>
+);
+
+export default Index;
